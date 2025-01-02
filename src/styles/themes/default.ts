0590af8dc10100c54a8043220be4511b0231c244
WesGabriel <wesgabriel79@gmail.com>
2025-01-02 20:31:25 -0300

    feat: configurando o setup do projeto

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,19 @@
+export const defaultTheme = {
+  white: "#fff",
+
+  blue: "#3294F8",
+  "base-title": "#E7EDF4",
+  "base-subtitle": "#C4D4E3",
+  "base-text": "#afc2d4",
+  "base-span": "#7b96b2",
+  "base-label": "#3a536b",
+  "base-border": "#1c2f41",
+  "base-post": "#112131",
+  "base-profile": "#0b1b2b",
+  "base-background": "#071422",
+  "base-input": "#040f1a",
+
+  "red-300": "#F75A68",
+  "red-500": "#AB222E",
+  "red-700": "#7A1921",
+} as const;
